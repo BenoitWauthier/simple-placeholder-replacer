a0f8d781eefbc722beae4ecf2714bd02a90e7bf7
BenoitWauthier <benoit.wauthier@gmail.com>
2021-03-15 11:10:42 +0100

    Extend test coverage

--- test/replace.spec.js
+++ test/replace.spec.js
@@ -8,39 +8,47 @@ describe('placeholderReplacer tests', function () {
     timekeeper.freeze(freezedDate)
 
     try {
+        it('Replace undefined / null input', function () {
+            assert.strictEqual(typeof(replace(undefined)), "undefined")
+            assert.strictEqual(replace(null), null)
+        })
+
         it('Replace NOW placeholder', function () {
-            assert.strictEqual(replace('key1: {{NOW=hh:mm:ss}}, key2: {{NOW}}'), `key1: ${calcMoment(0, 'd', 'hh:mm:ss')}, key2: ${calcMoment(0, 'd', 'YYYY-MM-DDTHH:mm:ss')}`)
-            assert.strictEqual(replace('key: {{now}}'), `key: ${calcMoment(0, 'd', 'YYYY-MM-DDTHH:mm:ss')}`)
-            assert.deepStrictEqual(replace(JSON.parse('{"key": "{{NOW}}"}')), { key: calcMoment(0, 'd', 'YYYY-MM-DDTHH:mm:ss') })
+            assertReplace('{"key1": "{{NOW=hh:mm:ss}}", "key2": "{{NOW}}"}', `{"key1": "${calcMoment(0, 'd', 'hh:mm:ss')}", "key2": "${calcMoment(0, 'd', 'YYYY-MM-DDTHH:mm:ss')}"}`)
+            assertReplace('{"key": "{{now}}"}', `{"key": "${calcMoment(0, 'd', 'YYYY-MM-DDTHH:mm:ss')}"}`)
         })
 
         it('Replkce TODAY placeholder', function () {
-            assert.strictEqual(replace('key1: {{TODAY}}, key2: {{TODAY=DD MM YYYY}}'), `key1: ${calcMoment()}, key2: ${calcMoment(0, 'd', 'DD MM YYYY')}`)
-            assert.strictEqual(replace('key: {{today}}'), `key: ${calcMoment(0, 'd', 'YYYY-MM-DD')}`)
-            assert.deepStrictEqual(replace(JSON.parse('{"key": "{{TODAY}}"}')), { key: calcMoment(0, 'd', 'YYYY-MM-DD') })
+            assertReplace('{"key1": "{{TODAY}}", "key2": "{{TODAY=DD MM YYYY}}"}', `{"key1": "${calcMoment()}", "key2": "${calcMoment(0, 'd', 'DD MM YYYY')}"}`)
+            assertReplace('{"key": "{{today}}"}', `{"key": "${calcMoment(0, 'd', 'YYYY-MM-DD')}"}`)
         })
 
         it('Replace TODAY+xxx placeholder', function () {
-            assert.strictEqual(replace('key1: {{TODAY+1}}, key2: {{TODAY+2=DD MM YYYY}}'), `key1: ${calcMoment(1, 'd', 'YYYY-MM-DD')}, key2: ${calcMoment(2, 'd', 'DD MM YYYY')}`)
-            assert.strictEqual(replace('key: {{today+1}}'), `key: ${calcMoment(1, 'd', 'YYYY-MM-DD')}`)
-            assert.strictEqual(replace('key: {{today+1M=DD MM YYYY}}'), `key: ${calcMoment(1, 'month', 'DD MM YYYY')}`)
-            assert.deepStrictEqual(replace(JSON.parse('{"key": "{{TODAY+1}}"}')), { key: calcMoment(1, 'd', 'YYYY-MM-DD') })
+            assertReplace('{"key1": "{{TODAY+1}}", "key2": "{{TODAY+2=DD MM YYYY}}"}', `{"key1": "${calcMoment(1, 'd', 'YYYY-MM-DD')}", "key2": "${calcMoment(2, 'd', 'DD MM YYYY')}"}`)
+            assertReplace('{"key": "{{today+1}}"}', `{"key": "${calcMoment(1, 'd', 'YYYY-MM-DD')}"}`)
+            assertReplace('{"key": "{{today+1M=DD MM YYYY}}"}', `{"key": "${calcMoment(1, 'month', 'DD MM YYYY')}"}`)
         })
 
         it('Replace TODAY-xxx placeholder', function () {
-            assert.strictEqual(replace('key1: {{TODAY-1}}, key2: {{TODAY-2=DD MM YYYY}}'), `key1: ${calcMoment(-1, 'd', 'YYYY-MM-DD')}, key2: ${calcMoment(-2, 'd', 'DD MM YYYY')}`)
-            assert.strictEqual(replace('key: {{today-1}}'), `key: ${calcMoment(-1, 'd', 'YYYY-MM-DD')}`)
-            assert.deepStrictEqual(replace(JSON.parse('{"key": "{{TODAY-1}}"}')), { key: calcMoment(-1, 'd', 'YYYY-MM-DD') })
+            assertReplace('{"key": "{{TODAY-1}}"}', `{"key": "${calcMoment(-1, 'd', 'YYYY-MM-DD')}"}`)
+            assertReplace('{"key": "{{TODAY-1M}}"}', `{"key": "${calcMoment(-1, 'month', 'YYYY-MM-DD')}"}`)
+            assertReplace('{"key1": "{{TODAY-1}}", "key2": "{{TODAY-2=DD MM YYYY}}"}', `{"key1": "${calcMoment(-1, 'd', 'YYYY-MM-DD')}", "key2": "${calcMoment(-2, 'd', 'DD MM YYYY')}"}`)
         })
 
         it('Replace custom placeholder', function () {
-            const placeholdersMapping = {
+            var ph3
+            const placeholders = {
                 ph1: "first",
-                ph2: "second"
+                ph2: "second",
+                ph3
             }
-            assert.strictEqual(replace('key1: {{ph1}}, key2: {{PH2}}', placeholdersMapping), 'key1: first, key2: second')
-            assert.deepStrictEqual(replace(JSON.parse('{"key1": "{{ph1}}", "key2": "{{PH2}}"}'), placeholdersMapping), { key1: "first", key2: "second" })
+            assertReplace('{"key1": "{{ph1}}", "key2": "{{PH2}}"}', '{"key1": "first", "key2": "second"}', placeholders)
         })
+
+        const assertReplace = (source, expected, placeholders) => {
+            assert.strictEqual(replace(source, placeholders), expected)
+            assert.deepStrictEqual(replace(JSON.parse(source), placeholders), JSON.parse(expected))
+        }
 
         const calcMoment = (number = 0, unit = 'd', format = 'YYYY-MM-DD') => {
             return moment().add(number, unit).format(format)
